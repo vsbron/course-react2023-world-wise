20bf5a2914624c5859e4e13376d4214ceb0cff1f
vsbron <vsbron@gmail.com>
2023-11-13 17:33:43 +0300

    Initial render of the countries list is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 import { useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:8000";
@@ -51,7 +52,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries:</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
 
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,39 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+function CountryList({ cities, isLoading }) {
+  // If list is still being loaded, display the spinner
+  if (isLoading) return <Spinner />;
+
+  // If cities array is empty, display the message
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  // Deriving cities list state to the countries list
+  // Reducing cties array and on each iteration checking
+  const countries = cities.reduce((arr, city) => {
+    // whether current city's country is in the country array
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      // If it's not - adding to the countries array
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else {
+      // If it is - leaving countries array untouched
+      return arr;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {/* Iterating through cities array and passing it to CityItem */}
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
